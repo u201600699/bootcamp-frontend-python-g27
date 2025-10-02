1bb9abe5ac0d0ab1285c323c89a106bfebc52696
Gabriel Carrion Marotta <u201600699@upc.edu.pe>
2025-10-02 12:41:43 -0500

    021025

--- Semana09/dia02-01-react-intro/src/App.jsx
+++ Semana09/dia02-01-react-intro/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,4 +32,12 @@ function App() {
   )
 }
 
+export default App */
+
+//01 -COMPONENTE BASE USANDO FUNCTION
+function App(){
+  return (
+    <h1> Vite + React</h1>
+  )
+}
 export default App
